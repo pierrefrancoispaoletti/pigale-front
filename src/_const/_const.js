e139ab547148b9a6bd2a1ecfea801752c6dbdce9
pierrefrancoispaoletti <pierre.paoletti1@gmail.com>
2021-06-24 23:43:03 +0200

    connecting to server

--- src/_const/_const.js
+++ src/_const/_const.js
@@ -1,6 +1,6 @@
 // export const $SERVER = "http://localhost:5000";
 
-export const $SERVER = "https://petitclos.herokuapp.com";
+export const $SERVER = "https://pigale.herokuapp.com";
 
 export const GOOGLE_API_KEY = "AIzaSyBlV19qRfHj05YOzkkavxXTS2HGx2D54Ps";
 
