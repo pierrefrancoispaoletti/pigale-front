ef5690edc2d752f4610e9946be76ea64ee37032e
pierrefrancoispaoletti <pierre.paoletti1@gmail.com>
2021-06-25 12:15:17 +0200

    adding env variables changing colors

--- src/_const/_const.js
+++ src/_const/_const.js
@@ -4,14 +4,14 @@ export const $SERVER = "https://pigale.herokuapp.com";
 
 export const GOOGLE_API_KEY = "";
 
-export const primary ="#FFCAAF"
+export const primary = "#8B0D32";
 
-export const secondary ="#FF6B6B"
+export const secondary = "#000000";
 
-export const ternary = "#B61919"
+export const ternary = "#FDD100";
 
-export const email = "sarl.lepigale@orange.fr"
-export const tel = "0495212046"
-export const facebook= "Bistrot-le-pigale-508686645856259"
-export const insta =""
-export const url = "https://bistrotlepigale.fr"
+export const email = "sarl.lepigale@orange.fr";
+export const tel = "0495212046";
+export const facebook = "Bistrot-le-pigale-508686645856259";
+export const insta = "";
+export const url = "https://bistrotlepigale.fr";
